bbf617b8b9386672d1d3c0b200ea321771bc3511
Razvan Stefan <razvanstefan@tudelft.nl>
2026-02-08 20:32:44 +0100

    refactored hexes and edges and linked them game logic

--- Catan/src/Catan.DebugClient/Views/CatanView.axaml.cs
+++ Catan/src/Catan.DebugClient/Views/CatanView.axaml.cs
@@ -35,6 +35,85 @@ namespace Catan.DebugClient.Views
                 allVertexControls[i].DataContext = model;
             }
 
+            var allHexControls = HexBoard.Children.OfType<HexTile>().ToList();
+            for (int i = 0; i < allHexControls.Count; i++)
+            {
+                var model = vm.AllTiles[i];
+                allHexControls[i].HexModel = model;
+                allHexControls[i].DataContext = model;
+            }
+
+            var allEdgeControls = new List<Edge>
+            {
+                edge1, edge2, edge3, edge4, edge5, edge6,
+                edge7, edge8, edge9, edge10, edge11, edge12,
+                edge13, edge14, edge15, edge16, edge17, edge18,
+                edge19, edge20, edge21, edge22, edge23, edge24,
+                edge25, edge26, edge27, edge28, edge29, edge30,
+                edge31, edge32, edge33, edge34, edge35, edge36,
+                edge37, edge38, edge39, edge40, edge41, edge42,
+                edge43, edge44, edge45, edge46, edge47, edge48,
+                edge49, edge50, edge51, edge52, edge53, edge54,
+                edge55, edge56, edge57, edge58, edge59, edge60,
+                edge61, edge62, edge63, edge64, edge65, edge66,
+                edge67, edge68, edge69, edge70, edge71, edge72
+            };
+
+            var edgeCoordinates = new (double X1, double Y1, double X2, double Y2)[]
+            {
+                (195, 22, 147.5, 50), (195, 20, 247.5, 50),
+                (295, 22, 247.5, 50), (295, 20, 347.5, 50),
+                (395, 22, 347.5, 50), (395, 20, 447.5, 50),
+                (147.5, 50, 147.5, 107.5), (247.5, 50, 247.5, 107.5),
+                (347.5, 50, 347.5, 107.5), (447.5, 50, 447.5, 107.5),
+                (147.5, 107.5, 95, 135.5), (147.5, 107.5, 195, 132.5),
+                (247.5, 107.5, 195, 135.5), (247.5, 107.5, 295, 132.5),
+                (347.5, 107.5, 295, 135.5), (347.5, 107.5, 395, 132.5),
+                (447.5, 107.5, 395, 135.5), (447.5, 107.5, 495, 132.5),
+                (100, 135.5, 100, 193), (200, 135.5, 200, 193),
+                (300, 135.5, 300, 193), (400, 135.5, 400, 193),
+                (500, 135.5, 500, 193), (100, 193, 47.5, 221),
+                (100, 193, 147.5, 221), (200, 193, 147.5, 221),
+                (200, 193, 247.5, 221), (300, 193, 247.5, 221),
+                (300, 193, 347.5, 221), (400, 193, 347.5, 221),
+                (400, 193, 447.5, 221), (500, 193, 447.5, 221),
+                (500, 193, 547.5, 221), (47.5, 221, 47.5, 276.5),
+                (147.5, 221, 147.5, 276.5), (247.5, 221, 247.5, 276.5),
+                (347.5, 221, 347.5, 276.5), (447.5, 221, 447.5, 276.5),
+                (547.5, 221, 547.5, 276.5), (47.5, 276.5, 100, 304.5),
+                (147.5, 276.5, 100, 304.5), (147.5, 276.5, 200, 304.5),
+                (247.5, 276.5, 200, 304.5), (247.5, 276.5, 300, 304.5),
+                (347.5, 276.5, 300, 304.5), (347.5, 276.5, 400, 304.5),
+                (447.5, 276.5, 400, 304.5), (447.5, 276.5, 500, 304.5),
+                (547.5, 276.5, 500, 304.5), (100, 304.5, 100, 362),
+                (200, 304.5, 200, 362), (300, 304.5, 300, 362), (400, 304.5, 400, 362),
+                (500, 304.5, 500, 362), (100, 362, 152.5, 390), (200, 362, 152.5, 390),
+                (200, 362, 252.5, 390), (300, 362, 252.5, 390), (300, 362, 352.5, 390),
+                (400, 362, 352.5, 390), (400, 362, 452.5, 390), (500, 362, 452.5, 390),
+                (152.5, 390, 152.5, 447), (252.5, 390, 252.5, 447),
+                (352.5, 390, 352.5, 447), (452.5, 390, 452.5, 447),
+                (152.5, 452, 205, 480), (252.5, 447, 205, 475), (252.5, 452, 305, 480),
+                (352.5, 447, 305, 475), (352.5, 452, 405, 480), (452.5, 447, 405, 475)
+
+            };
+            
+            for (int i = 0; i < allEdgeControls.Count; i++)
+            {
+                var control = allEdgeControls[i];
+                var gameEdge = vm.AllEdges[i].GameEdge;
+
+                var model = vm.AllEdges[i];
+                model.StartPoint = new Avalonia.Point(edgeCoordinates[i].X1, edgeCoordinates[i].Y1);
+                model.EndPoint = new Avalonia.Point(edgeCoordinates[i].X2, edgeCoordinates[i].Y2);
+
+                model.OnClicked = (edgeModel) =>
+                {
+                    vm.TryPlaceRoad(edgeModel.GameEdge);
+                };
+
+                control.DataContext = model;
+            }
+
             BtnSettlements.Click += (sender, e) => vm.OnSettlementButtonClicked();
 
             vm.RefreshPlayers();
--- Catan/src/Catan.DebugClient/Views/CatanViewModel.cs
+++ Catan/src/Catan.DebugClient/Views/CatanViewModel.cs
@@ -1,6 +1,4 @@
 using System.Collections.ObjectModel;
-using Avalonia.Controls;
-using Avalonia.Threading;
 using Avalonia.Media;
 using Catan.Shared.Game;
 using System;
@@ -16,15 +14,18 @@ namespace Catan.DebugClient.Views
         City,
         Road
     }
-    
+
     public class CatanViewModel : ViewModelBase
     {
-        public GameSession Game { get;}
+        // ================= PROPERTIES =================
+
+        public GameSession Game { get; }
         public Dictionary<Player, IBrush> PlayerColors { get; } = new();
-
         public PlacementType CurrentPlacement { get; private set; } = PlacementType.None;
-
+        
+        public ObservableCollection<HexTileModel> AllTiles { get; set; } = new();
         public ObservableCollection<VertexModel> AllVertices { get; set; } = new();
+        public ObservableCollection<EdgeModel> AllEdges { get; set; } = new();
 
         private string _gameLog = "";
         public string GameLog
@@ -33,28 +34,7 @@ namespace Catan.DebugClient.Views
             set { _gameLog = value; RaisePropertyChanged(nameof(GameLog)); }
         }
 
-        public void AppendToGameLog(string message)
-        {
-            GameLog += message + "\n";
-        }
-
-        public CatanViewModel(GameSession game)
-        {
-            Game = game;
-            // Link colors to each player
-            var colors = new[] { Brushes.Blue, Brushes.Green, Brushes.Red, Brushes.Orange };
-            for (int i = 0; i < Game.Players.Count; i++)
-                PlayerColors[Game.Players[i]] = colors[i];
-
-            // Link UI vertex to Game vertex
-            foreach (var vertex in Game.Board.Vertices)
-            {
-                AllVertices.Add(new VertexModel(vertex, PlayerColors));
-            }
-
-            AppendToGameLog($"Player {CurrentPlayerName}'s turn to build initial settlement!");
-        }
-
+        // Player info for UI
         public string CurrentPlayerName => Game.GetCurrentPlayer().Username;
         public string Player1Name => Game.Players[0].Username;
         public string Player2Name => Game.Players[1].Username;
@@ -97,30 +77,39 @@ namespace Catan.DebugClient.Views
         public string Brick => Game.GetCurrentPlayer().Resources[ResourceType.Brick].ToString();
         public string Wood => Game.GetCurrentPlayer().Resources[ResourceType.Wood].ToString();
 
-        // --- Commands for Buttons ---
+        // ================= CONSTRUCTOR =================
 
-        public async void RollDice()
+        public CatanViewModel(GameSession game)
         {
-            var roll = Random.Shared.Next(1, 7) + Random.Shared.Next(1, 7);
+            Game = game;
 
-            // TODO: call your board/resource distribution logic here
+            // Assign colors to players by order
+            var colors = new[] { Brushes.Blue, Brushes.Green, Brushes.Red, Brushes.Orange };
+            for (int i = 0; i < Game.Players.Count; i++)
+                PlayerColors[Game.Players[i]] = colors[i];
 
-            Game.Turn = GameSession.TurnPhase.Trade;
+            // Create VertexModels for all board vertices
+            foreach (var vertex in Game.Board.Vertices)
+                AllVertices.Add(new VertexModel(vertex, PlayerColors));
+            foreach (var edge in Game.Board.Edges)
+                AllEdges.Add(new EdgeModel(edge, PlayerColors));
+            foreach (var tile in Game.Board.Tiles)
+                AllTiles.Add(new HexTileModel(tile));
 
-            // Notify UI that things changed
-            RaisePropertyChanged(nameof(Wheat));
-            RaisePropertyChanged(nameof(Sheep));
-            RaisePropertyChanged(nameof(Stone));
-            RaisePropertyChanged(nameof(Brick));
-            RaisePropertyChanged(nameof(Wood));
+            if (Game.Phase == GameSession.GamePhase.SetupRound1 || Game.Phase == GameSession.GamePhase.SetupRound2)
+            {
+                CurrentPlacement = PlacementType.Settlement;
+                AppendToGameLog($"Player {CurrentPlayerName}'s turn to place initial settlement!");
+                HighlightInitialValidVertices();
+            }
+            AppendToGameLog($"Player {CurrentPlayerName}'s turn to build initial settlement!");
         }
 
-        public void OnSettlementButtonClicked()
-        {
-            CurrentPlacement = PlacementType.Settlement;
-            AppendToGameLog($"{CurrentPlayerName} is placing a settlement. Click a highlighted vertex.");
+        // ================= UI HELPER FUNCTIONS =================
 
-            HighlightInitialValidVertices();
+        public void AppendToGameLog(string message)
+        {
+            GameLog += message + "\n";
         }
 
         public void RefreshPlayers()
@@ -146,20 +135,48 @@ namespace Catan.DebugClient.Views
             RaisePropertyChanged(nameof(Player4Points));
         }
 
-        private IBrush GetPlayerColor(Player player)
+        public async void RollDice()
         {
-            var index = Game.Players.IndexOf(player);
-            return index switch
-            {
-                0 => Brushes.Blue,
-                1 => Brushes.Green,
-                2 => Brushes.Red,
-                3 => Brushes.Orange,
-                _ => Brushes.Gray
-            };
+            var roll = Random.Shared.Next(1, 7) + Random.Shared.Next(1, 7);
+
+            // TODO: call resource distribution logic
+
+            Game.Turn = GameSession.TurnPhase.Trade;
+
+            RaisePropertyChanged(nameof(Wheat));
+            RaisePropertyChanged(nameof(Sheep));
+            RaisePropertyChanged(nameof(Stone));
+            RaisePropertyChanged(nameof(Brick));
+            RaisePropertyChanged(nameof(Wood));
         }
 
-        // --- SETTLEMENT PLACEMENT LOGIC ---
+        // ================= PLACEMENT BUTTONS =================
+
+        public void OnSettlementButtonClicked()
+        {
+            if (Game.Phase != GameSession.GamePhase.SetupRound1 && Game.Phase != GameSession.GamePhase.SetupRound2)
+            {
+                CurrentPlacement = PlacementType.Settlement;
+                AppendToGameLog($"{CurrentPlayerName} is placing a settlement. Click a highlighted vertex.");
+                HighlightValidVertices();
+            }
+        }
+
+        public void OnCityButtonClicked()
+        {
+            CurrentPlacement = PlacementType.City;
+            AppendToGameLog($"{CurrentPlayerName} is placing a city. Click a valid vertex.");
+            HighlightValidCities();
+        }
+
+        public void OnRoadButtonClicked()
+        {
+            CurrentPlacement = PlacementType.Road;
+            AppendToGameLog($"{CurrentPlayerName} is placing a road. Click a valid edge.");
+            HighlightValidEdges();
+        }
+
+        // ================= SETTLEMENT PLACEMENT LOGIC =================
 
         private void HighlightInitialValidVertices()
         {
@@ -169,46 +186,61 @@ namespace Catan.DebugClient.Views
                                          CanBuildInitialSettlement(vertex.GameVertex);
             }
         }
+        private void HighlightValidVertices()
+        {
+            foreach (var vertex in AllVertices)
+            {
+                vertex.IsHighlightable = CurrentPlacement == PlacementType.Settlement &&
+                                         CanBuildSettlement(vertex.GameVertex);
+            }
+        }
+
+        private void UnhighlightVertices()
+        {
+            foreach (var vertex in AllVertices)
+            {
+                vertex.IsHighlightable = false;
+            }
+        }
 
         public void TryPlaceSettlement(VertexModel vertexModel)
         {
             if (CurrentPlacement != PlacementType.Settlement)
                 return;
-            if(Game.Phase == GameSession.GamePhase.SetupRound1 || Game.Phase == GameSession.GamePhase.SetupRound2)
+
+            bool success = false;
+
+            if (Game.Phase == GameSession.GamePhase.SetupRound1 || Game.Phase == GameSession.GamePhase.SetupRound2)
             {
-                if (CanBuildInitialSettlement(vertexModel.GameVertex))
-                {
-                    vertexModel.GameVertex.IsSettlement = true;
-                    vertexModel.GameVertex.Owner = Game.GetCurrentPlayer();
+                success = CanBuildInitialSettlement(vertexModel.GameVertex);
+            }
+            else
+            {
+                success = CanBuildSettlement(vertexModel.GameVertex);
+            }
 
-                    AppendToGameLog($"{CurrentPlayerName} built a settlement at vertex #{vertexModel.GameVertex.Index}");
-
-                    CurrentPlacement = PlacementType.None;
-                }
-                else
+            if (success)
+            {
+                vertexModel.GameVertex.IsSettlement = true;
+                vertexModel.GameVertex.Owner = Game.GetCurrentPlayer();
+                AppendToGameLog($"{CurrentPlayerName} built a settlement at vertex #{vertexModel.GameVertex.Index}");
+                if (Game.Phase == GameSession.GamePhase.SetupRound1 || Game.Phase == GameSession.GamePhase.SetupRound2)
                 {
-                    AppendToGameLog($"{CurrentPlayerName} cannot build a settlement at vertex #{vertexModel.GameVertex.Index}. Press 'Settlement' again to retry.");
-                    CurrentPlacement = PlacementType.None;
+                    CurrentPlacement = PlacementType.Road;
+                    AppendToGameLog($"{CurrentPlayerName} must now place a road connected to the settlement.");
+                    HighlightInitialValidEdges(vertexModel.GameVertex);
+                    UnhighlightVertices();
+                    return;
                 }
             }
             else
             {
-                if (CanBuildSettlement(vertexModel.GameVertex))
-                {
-                    vertexModel.GameVertex.IsSettlement = true;
-                    vertexModel.GameVertex.Owner = Game.GetCurrentPlayer();
-
-                    AppendToGameLog($"{CurrentPlayerName} built a settlement at vertex #{vertexModel.GameVertex.Index}");
-
-                    CurrentPlacement = PlacementType.None;
-                }
-                else
-                {
-                    AppendToGameLog($"{CurrentPlayerName} cannot build a settlement at vertex #{vertexModel.GameVertex.Index}. Press 'Settlement' again to retry.");
-                    CurrentPlacement = PlacementType.None;
-                }
+                AppendToGameLog($"{CurrentPlayerName} cannot build a settlement at vertex #{vertexModel.GameVertex.Index}. Press 'Settlement' again to retry.");
             }
-            
+
+            CurrentPlacement = PlacementType.None;
+
+            // Reset highlights
             foreach (var v in AllVertices)
                 v.IsHighlightable = false;
 
@@ -217,14 +249,64 @@ namespace Catan.DebugClient.Views
 
         private bool CanBuildInitialSettlement(Shared.Game.Vertex vertex)
         {
-            var result = Game.Board.CanPlaceSettlement(Game.GetCurrentPlayer(), vertex, true);
-            return result == Board.SettlementPlacementResult.Success;
+            return Game.Board.CanPlaceSettlement(Game.GetCurrentPlayer(), vertex, true) ==
+                   Board.SettlementPlacementResult.Success;
         }
 
         private bool CanBuildSettlement(Shared.Game.Vertex vertex)
         {
-            var result = Game.Board.CanPlaceSettlement(Game.GetCurrentPlayer(), vertex, false);
-            return result == Board.SettlementPlacementResult.Success;
+            return Game.Board.CanPlaceSettlement(Game.GetCurrentPlayer(), vertex, false) ==
+                   Board.SettlementPlacementResult.Success;
         }
+
+        // ================= ROAD LOGIC =================
+
+        private void HighlightInitialValidEdges(Shared.Game.Vertex vertex)
+        {
+            foreach (var edge in AllEdges)
+            {
+                edge.IsHighlightable = CurrentPlacement == PlacementType.Road &&
+                                         CanBuildInitialRoad(edge.GameEdge, vertex);
+                if(edge.GameEdge.Index == 0)
+                    AppendToGameLog($"{edge.IsHighlightable}");
+            }
+        }
+        private void HighlightValidEdges()
+        {
+            foreach (var edge in AllEdges)
+            {
+                edge.IsHighlightable = CurrentPlacement == PlacementType.Road &&
+                                         CanBuildRoad(edge.GameEdge);
+            }
+        }
+        public void TryPlaceRoad(Shared.Game.Edge edge)
+        {
+        }
+
+        private bool CanBuildInitialRoad(Shared.Game.Edge edge, Shared.Game.Vertex vertex)
+        {
+            int[] connectedVertices = BoardMappings.EdgeMapping[edge.Index];
+            
+            return connectedVertices[0] == vertex.Index || connectedVertices[1] == vertex.Index;
+        }
+
+        private bool CanBuildRoad(Shared.Game.Edge edge)
+        {
+            return Game.Board.CanPlaceRoad(Game.GetCurrentPlayer(), edge) ==
+                   Board.RoadPlacementResult.Success;
+        }
+
+        // ================= CITY LOGIC =================
+        private void HighlightValidCities()
+        {
+        }
+        public void TryPlaceCity(Shared.Game.Vertex vertex)
+        {
+        }
+        private bool CanBuildCity(Shared.Game.Vertex vertex)
+        {
+            return true;
+        }
+        // ================= DEVELOPMENT CARD LOGIC =================
     }
 }
